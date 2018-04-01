bce44800e154f062f55a63a24cf3dac9773ee3e3
r.speransky <>
2018-04-01 17:55:13 +0300

    Created BaseTest and InitDriver

---
+++ src/main/java/helpers/BaseTest.kt
@@ -0,0 +1,19 @@
+package main.java.helpers
+
+import com.codeborne.selenide.Configuration
+import com.codeborne.selenide.Selenide
+import com.codeborne.selenide.WebDriverRunner
+import org.junit.jupiter.api.AfterEach
+
+class BaseTest {
+
+    init {
+        Configuration.browser = InitDriverProvider()::class.java.name
+    }
+
+    @AfterEach
+    fun clearCaches() {
+        WebDriverRunner.clearBrowserCache()
+        Selenide.clearBrowserCookies()
+    }
+}
