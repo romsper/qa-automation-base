d9b6b9098a76379f7745d651a5087b555f605726
romsper <r.speranskii@gmail.com>
2024-12-27 01:50:54 -0500

    Update DynamicTablePage.kt

--- src/main/kotlin/pages/DynamicTablePage.kt
+++ src/main/kotlin/pages/DynamicTablePage.kt
@@ -25,8 +25,8 @@ class DynamicTablePage {
 }
 
 // This class is a wrapper for the locator of the item in the list
-// Bug in Playwright: https://github.com/microsoft/playwright/issues/12529
-
+// Similar bug in Playwright: https://github.com/microsoft/playwright/issues/12529
+// Reported in NEW issue: https://github.com/microsoft/playwright/issues/34147
 class Superhero(item: Locator) {
     val name = item.locator(".div[@class='ml-4']/div[1]")
     val email = item.locator(".//div[@class='ml-4']/div[2]")
